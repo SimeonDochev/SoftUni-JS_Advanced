61ca9e1eb128bec4c91bbd285c5938728eb2c1c3
Simeon Dochev <simeon.dochev16@gmail.com>
2021-10-14 18:02:40 +0300

    upload solved problem

---
+++ Prototypes_and_Inheritance_Exercise/03-extensible-object.js
@@ -0,0 +1,22 @@
+function extensibleObject() {
+    return {
+        extend: function(template) {
+            const objProto = Object.getPrototypeOf(this);
+            for (const [key, value] of Object.entries(template)) {
+                if (typeof value == 'function') {
+                    objProto[key] = value;
+                } else {
+                    this[key] = value;
+                }
+            }
+        }
+    }
+}
+
+const myObj = extensibleObject();
+const template = { 
+    extensionMethod: function () {}, 
+    extensionProperty: 'someString' 
+} 
+myObj.extend(template); 
+console.log(myObj);
