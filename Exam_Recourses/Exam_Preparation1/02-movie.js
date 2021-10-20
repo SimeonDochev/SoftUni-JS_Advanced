0bffeaef3bdeda2b1a7c605d8e02e68f356fa8de
Simeon Dochev <simeon.dochev16@gmail.com>
2021-10-20 19:12:12 +0300

    exam prep

---
+++ Exam_Recourses/Exam_Preparation1/02-movie.js
@@ -0,0 +1,49 @@
+class Movie {
+    constructor(movieName, ticketPrice) {
+        this.movieName = movieName;
+        this.ticketPrice = ticketPrice;
+        this.screenings = [];
+        this.totalProfit = 0;
+        this.totalSoldTickets = 0;
+    }
+    newScreening(date, hall, description) {
+        for (let screening of this.screenings) {
+            if (screening['date'] == date && screening['hall'] == hall) {
+                throw new Error(`Sorry, ${hall} hall is not available on ${date}`);
+            }
+        }
+        this.screenings.push({
+            date,
+            hall,
+            description
+        });
+        return `New screening of ${this.movieName} is added.`;
+    }
+    endScreening(date, hall, soldTickets) {
+        for (let screening of this.screenings) {
+            if (screening['date'] == date && screening['hall'] == hall) {
+                this.totalProfit += soldTickets * this.ticketPrice;
+                this.totalSoldTickets += soldTickets;
+                const idx = this.screenings.indexOf(screening);
+                this.screenings.splice(idx, 1);
+                return `${this.movieName} movie screening on ${date} in ${hall} hall has ended. Screening profit: ${soldTickets * this.ticketPrice}`;
+            }
+        }
+        throw new Error(`Sorry, there is no such screening for ${this.movieName} movie.`);
+    }
+    toString() {
+        let result = `${this.movieName} full information:\n`;
+        result += `Total profit: ${this.totalProfit.toFixed(0)}$\n`;
+        result += `Sold Tickets: ${this.totalSoldTickets}\n`;
+        if (this.screenings.length > 0) {
+            result += 'Remaining film screenings:\n';
+            this.screenings.sort((a, b) => a['hall'].localeCompare(b['hall']));
+            for (let screening of this.screenings) {
+                result += `${screening['hall']} - ${screening['date']} - ${screening['description']}\n`;
+            }
+        } else {
+            result += 'No more screenings!';
+        }
+        return result.trim();
+    }
+}
--- Exam_Recourses/Exam_Preparation1/testNumbers.js
+++ Exam_Recourses/Exam_Preparation1/testNumbers.js
@@ -1,3 +1,5 @@
+const { expect } = require('chai');
+
 const testNumbers = {
     sumNumbers: function (num1, num2) {
         let sum = 0;
@@ -35,5 +37,44 @@ const testNumbers = {
     }
 };
 
-
-
+describe('Test Numbers', function() {
+    describe('sumNumber tests', function() {
+        it('returns undefined when first parameter is not a number', function() {
+            expect(testNumbers.sumNumbers('1', 1)).to.be.undefined;
+        });
+        it('returns undefined when second parameter is not a number', () => {
+            expect(testNumbers.sumNumbers(1, '1')).to.be.undefined;
+        });
+        it('works with positive numbers', () => {
+            expect(testNumbers.sumNumbers(1, 1)).to.equal('2.00');
+        });
+        it('works with negative numbers', () => {
+            expect(testNumbers.sumNumbers(-1, -2)).to.equal('-3.00');
+        });
+        it('works with a negative and a positive number', () => {
+            expect(testNumbers.sumNumbers(1, -2)).to.equal('-1.00');
+        });
+    });
+    describe('numberChecker tests', () => {
+        it('returns the correct result with even number', () => {
+            expect(testNumbers.numberChecker(2)).to.equal('The number is even!');
+        });
+        it('returns the correct result with odd number', () => {
+            expect(testNumbers.numberChecker(1)).to.equal('The number is odd!');
+        });
+        it('works with a number passed as a string', () => {
+            expect(testNumbers.numberChecker('1')).to.equal('The number is odd!');
+        });
+        it('throws an error with incorrect parameter', () => {
+            expect(() => {testNumbers.numberChecker('asd')}).to.throw('The input is not a number!');
+        });
+    });
+    describe('averageSumArray tests', () => {
+        it('returns the correct result', () => {
+            expect(testNumbers.averageSumArray([1, 1, 1])).to.equal(1);
+        });
+        it('starts from zero', () => {
+            expect(testNumbers.averageSumArray([0])).to.equal(0);
+        });
+    })
+});
